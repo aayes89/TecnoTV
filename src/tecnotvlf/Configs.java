df9bbd10a52d557a15370b2f7211bee58d4f2bcd
Allan (Slam) <72238104+aayes89@users.noreply.github.com>
2024-03-20 19:40:10 -0600

    Add files via upload

---
+++ src/tecnotvlf/Configs.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tecnotvlf;
+
+/**
+ *
+ * @author localadmin
+ */
+public class Configs {
+
+    public static final String mainUrl = "https://spinoff.link/listas-premium/";
+    public static final String canal6 = "https://mdstrm.com/live-stream-playlist/5f2d9d6ff17144074bd8a284.m3u8";
+    public static final String urlRoku = "http://192.168.1.27:8060";
+    public static final String param = "/launch/63218?live=false&autoCookie=false&debugVideoHud=false&url=";
+    public static final String param2 = "&fmt=Auto&drmParams=%7B%7D&headers=%7B%7D&metada=%7B%22isFullHD%22%3Afalse%7D&cookies=%5B%5D";
+    
+    public Configs() {
+    }
+}
